d36331c8108acca8f153e5ca2bd89e7ddec2f280
turtle-insect <turtle.insect@gmail.com>
2026-09-17 05:07:44 +0900

    refactor

--- TrailsintheSky2ndChapter/Model/GeneralModel.cs
+++ TrailsintheSky2ndChapter/Model/GeneralModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TrailsintheSky2ndChapter.Model
+﻿namespace TrailsintheSky2ndChapter.Model
 {
 	internal class GeneralModel
 	{
