92e3ac2740ac6dc84e22e27c9ca150aacb3f681f
turtle-insect <turtle.insect@gmail.com>
2026-09-17 05:05:15 +0900

    refactor

--- TrailsintheSky2ndChapter/ActionCommand.cs
+++ TrailsintheSky2ndChapter/ActionCommand.cs
@@ -5,14 +5,12 @@ using System.Windows.Input;
 
 namespace TrailsintheSky2ndChapter
 {
-	internal class ActionCommand : ICommand
+	internal class ActionCommand(Action<object?> _action) : ICommand
 	{
-		private readonly Action<object?> mAction;
 #pragma warning disable CS0067
 		public event EventHandler? CanExecuteChanged;
 #pragma warning restore CS0067
 		public bool CanExecute(object? parameter) => true;
-		public ActionCommand(Action<object?> action) => mAction = action;
-		public void Execute(object? parameter) => mAction(parameter);
+		public void Execute(object? parameter) => _action(parameter);
 	}
 }
